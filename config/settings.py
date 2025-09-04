1af6d126541c8c045553c431bb3d7cf31f78b5d9
ExITPerson <baranovs11@yandex.ru>
2025-09-05 00:23:12 +0300

    Задание 3

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 import os
 
-from django.conf.global_settings import AUTH_USER_MODEL
+from django.conf.global_settings import AUTH_USER_MODEL, LOGIN_URL, LOGOUT_REDIRECT_URL
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -106,3 +106,6 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 AUTH_USER_MODEL = 'users.User'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from users.models import User
+from users.forms import UserRegisterForm
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('login.html')
